46058f37a7f0a120c24da4ba6612548873f7ef12
Lukas Koning <lfkoning@gmail.com>
2021-03-29 15:11:16 +0200

    Initial commit

---
+++ validator.py
@@ -0,0 +1,93 @@
+import logging
+
+from os import access, R_OK
+from lxml import etree
+
+
+class XMLValidator:
+    """
+    Validates XML files using the provided XSD schema.
+
+    Parameters
+    ----------
+    xsd_path : str
+        Path to the file containing the XSD schema.
+    valid_dtd : Optional[bool
+        Also validate the DTD (default) or not.
+    """
+
+    def __init__(self, xsd_path, valid_dtd=True):
+        self._logger = logging.getLogger(__name__)
+        self._valid_dtd = valid_dtd
+        self._schema = self._read_xsd(xsd_path)
+
+    def validate(self, xml_paths):
+        """
+        Validate a set of XML files using the provided schema.
+        """
+
+        # TODO: parallelize with joblib if needed
+        for xml_path in xml_paths:
+            if not self._is_readable(xml_path):
+                # Log warning but continue with the remaining files
+                self._logger.warning(f"Cannot read from XML file: {xml_path}")
+                continue
+
+            self._logger.info(f"PROCESSING:   [{xml_path}]")
+            xml_doc = etree.parse(xml_path)
+            valid_errors = []
+            try:
+                self._schema.assertValid(xml_doc)
+            except etree.DocumentInvalid:
+                valid_errors = [error for error in self._schema.error_log]
+
+            if valid_errors:
+                for error in self._schema.error_log:
+                    self._logger.error(f"ERROR:        [{xml_path} - L:{error.line}] - {error.message}")
+            else:
+                self._logger.info(f"SUCCESS:      [{xml_path}] Validation was succesful!")
+
+            self._logger.info(f"FINISHED:     [{xml_path}]")
+
+
+    def _read_xsd(self, xsd_path):
+        """Reads and parses an XSD schema from the provided file path.
+
+        Parameters
+        ----------
+        xsd_path : str
+            Path to the XSD schema file.
+
+        Returns
+        -------
+        lxml.etree.XMLSchema
+            Parsed XML tree of the XSD schema.
+        """
+
+        if not self._is_readable(xsd_path):
+            raise RuntimeError("Cannot read from XSD file path.")
+
+        with open(xsd_path, "r") as xsd_file:
+            return etree.XMLSchema(file=xsd_file)
+
+    def _is_readable(self, file_path):
+        """
+        Checks whether a file is readable or not.
+
+        Parameters
+        ----------
+        file_path : str
+            Path to the file to check.
+
+        Returns
+        -------
+        bool
+            Boolean indicating whether the file is readable.
+        """
+
+        readable = access(file_path, R_OK)
+
+        if not readable:
+            self._logger.warning(f"Could not read file: {file_path}")
+
+        return readable
